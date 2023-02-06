4b3b103f7966019da03beec43ec0a4e046a533af
Ernest <onetwo20003@gmail.com>
2023-02-07 01:44:37 +0500

    basic admin panel added

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from core.models import Service
+
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    """
+    Model to display Services in admin panel.
+    """
+    list_display = ('name', 'slug_name', 'state', 'created_at')
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).order_by('slug_name', '-created_at').distinct('slug_name')
